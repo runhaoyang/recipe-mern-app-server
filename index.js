9edb8241d18795d65871df1bea85d92c42179912
Runhao Yang <runhao@Runhaos-MacBook-Pro.local>
2022-03-01 21:51:45 -0500

    testing

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mongoose = require("mongoose");
-const userModel = require("./models");
 
 const app = express();
 const port = 3001;
 
 app.use(cors());
 
-mongoose.connect(
-  "mongodb+srv://admin:Pass123@cluster0.yahvt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-);
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
-
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/details", async (req, res) => {
-  const user = await userModel.find({});
-  res.send(user);
+  res.send("In the details page.");
 });
 
 app.listen(port, () =>
