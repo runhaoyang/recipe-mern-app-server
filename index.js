1b017198a9791f7672bb20610d99948a5c185e12
Runhao Yang <runhao@Runhaos-MacBook-Pro.local>
2022-03-02 09:45:52 -0500

    changed port configs

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.get("/details", async (req, res) => {
   res.send(user);
 });
 
-app.listen(port, () =>
+app.listen(process.env.PORT || 5000), () =>
   console.log(`Hello world app listening on port ${port}!`)
 );
