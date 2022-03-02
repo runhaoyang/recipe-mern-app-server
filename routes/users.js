8ab98178b028f7cd656de7ca591b0f7c269ebd27
Runhao Yang <runhao@Runhaos-MacBook-Pro.local>
2022-03-02 14:50:05 -0500

    cors

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Users = require("../models/Users");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
 
 router.get("/", async (req, res) => {
   const user = await Users.find({});
