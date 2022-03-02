f3cd95f751ab787e8f0751a5984ff93d734e648a
Runhao Yang <runhao@Runhaos-MacBook-Pro.local>
2022-03-01 21:53:29 -0500

    a

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
 const app = express();
-const port = 3001;
+const port = 5000;
 
-app.use(cors());
+app.get("/details", (req, res) => res.send("In the details page."));
 
-// Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.get("/details", async (req, res) => {
-  res.send("In the details page.");
-});
-
-app.listen(port, () =>
-  console.log(`Hello world app listening on port ${port}!`)
+app.listen(process.env.PORT || port, () =>
+  console.log(`Example app listening at ${port}`)
 );
