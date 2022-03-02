d72da2198915088b989b562c2ff760efa4e21c5f
Runhao Yang <runhao@Runhaos-MacBook-Pro.local>
2022-03-02 09:15:21 -0500

    added models and configured index

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const userModel = require("./models");
+
+const app = express();
 const port = 5000;
 
-const Users = [
-  {
-    id: 1002,
-    username: "decode",
-    age: 30,
-  },
-  {
-    id: 1003,
-    username: "otherUser",
-    age: 41,
-  },
-];
+app.use(cors());
 
-app.use(
-  cors({
-    origin: "*",
-  })
+mongoose.connect(
+  "mongodb+srv://admin:Pass123@cluster0.yahvt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
-app.get("/details", (req, res) => res.send(Users));
 
-app.listen(process.env.PORT || port, () =>
-  console.log(`Example app listening at ${port}`)
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/details", async (req, res) => {
+  const user = await userModel.find({});
+  res.send(user);
+});
+
+app.listen(port, () =>
+  console.log(`Hello world app listening on port ${port}!`)
 );
