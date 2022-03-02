b7719b8258b49c7b2b8743dc0521e716ccda1ea0
Runhao Yang <runhao@Runhaos-MacBook-Pro.local>
2022-03-01 22:38:50 -0500

    added testing json

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const app = express();
 const cors = require("cors");
 const port = 5000;
 
+const Users = [
+  {
+    id: 1002,
+    username: "decode",
+    age: 30,
+  },
+  {
+    id: 1003,
+    username: "otherUser",
+    age: 41,
+  },
+];
+
 app.use(
   cors({
     origin: "*",
   })
 );
-app.get("/details", (req, res) => res.send("In the details page."));
+app.get("/details", (req, res) => res.send(Users));
 
 app.listen(process.env.PORT || port, () =>
   console.log(`Example app listening at ${port}`)
