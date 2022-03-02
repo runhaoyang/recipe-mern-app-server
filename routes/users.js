b40bc368af0930940fb563873d79953e181e62f6
Runhao Yang <runhao@Runhaos-MacBook-Pro.local>
2022-03-02 13:32:11 -0500

    fixed models

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Users = require("./models");
+const Users = require("../models/Users");
 
 router.get("/", async (req, res) => {
   const user = await Users.find({});
