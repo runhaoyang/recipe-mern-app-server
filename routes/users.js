439175ac217a01c25a3f416fb5723c53ecf6dbdd
Runhao Yang <runhao@Runhaos-MacBook-Pro.local>
2022-03-02 15:03:34 -0500

    ssl-redirect

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,9 @@ const express = require("express");
 const router = express.Router();
 const Users = require("../models/Users");
 const cors = require("cors");
-import sslRedirect from "heroku-ssl-redirect";
+
 const app = express();
 app.use(cors());
-app.use(sslRedirect());
 
 router.get("/", async (req, res) => {
   const user = await Users.find({});
