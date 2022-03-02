5f0b196bb76d79ca3f981b6d2e5abc204ff6b620
Runhao Yang <runhao@Runhaos-MacBook-Pro.local>
2022-03-01 21:41:04 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,37 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const userModel = require("./models");
-
 const app = express();
 const port = 5000;
 
-app.use(cors());
+app.get("/", (req, res) => res.send("Hello World!"));
 
-mongoose.connect(
-  "mongodb+srv://admin:Pass123@cluster0.yahvt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
-
-// Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.get("/details", async (req, res) => {
-  const user = await userModel.find({});
-  res.send(user);
-});
-
-app.listen(port, () =>
-  console.log(`Hello world app listening on port ${port}!`)
+app.listen(process.env.PORT || port, () =>
+  console.log(`Example app listening at ${port}`)
 );
